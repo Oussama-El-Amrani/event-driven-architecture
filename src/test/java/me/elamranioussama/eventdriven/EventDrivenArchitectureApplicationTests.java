ca2b5fa2fd4ebe1f71003fda395c4c29013d12bc
Ousssama El-Amrani <oussama.maroc8@gmail.com>
2025-12-26 00:57:28 +0100

    fix: init project with their dependencies

---
+++ src/test/java/me/elamranioussama/eventdriven/EventDrivenArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elamranioussama.eventdriven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
