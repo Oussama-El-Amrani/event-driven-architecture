3f44b2813ae85b005baf3fce416d15f1721b786c
Ousssama El-Amrani <oussama.maroc8@gmail.com>
2025-12-27 23:52:11 +0100

    fix(Backend): update image tag and refine KStream window duration

--- src/main/java/me/elamranioussama/eventdriven/handlers/PageEventHandler.java
+++ src/main/java/me/elamranioussama/eventdriven/handlers/PageEventHandler.java
@@ -40,12 +40,12 @@ public class PageEventHandler {
     }
 
     @Bean
-    public Function<KStream<String, PageEvent>,KStream<String, Long>> KStreamFunction() {
+    public Function<KStream<String, PageEvent>,KStream<String, Long>> kStreamFunction() {
         return (input) -> input
                 .filter((k,v) -> v.duration() > 100)
                 .map((k,v) -> new KeyValue<>(v.name(), v.duration()))
                 .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
-                .windowedBy(TimeWindows.of(Duration.ofSeconds(50000)))
+                .windowedBy(TimeWindows.of(Duration.ofSeconds(5000)))
                 .count(Materialized.as("count-store"))
                 .toStream()
                 .map((k,v)-> new KeyValue<>(k.key(), v));
