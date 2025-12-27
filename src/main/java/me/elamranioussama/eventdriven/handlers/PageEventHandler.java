131af33b6f01fbcae89e001833a42b5363269bd1
Ousssama El-Amrani <oussama.maroc8@gmail.com>
2025-12-27 14:03:44 +0100

    feat: add pageEventSupplier

---
+++ src/main/java/me/elamranioussama/eventdriven/handlers/PageEventHandler.java
@@ -0,0 +1,33 @@
+package me.elamranioussama.eventdriven.handlers;
+
+import me.elamranioussama.eventdriven.events.PageEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@Component
+public class PageEventHandler {
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer() {
+        return (input) -> {
+            System.out.println("_________________________________");
+            System.out.println(input.toString());
+            System.out.println("_________________________________");
+        };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier() {
+        return () ->
+                new PageEvent(
+                        Math.random() > 0.5 ? "P1" : "P2",
+                        Math.random() > 0.5 ? "U1" : "U2",
+                        new Date(),
+                        10 + new Random().nextLong(10000)
+                );
+    }
+}
