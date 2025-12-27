95ad96dda212ececd589540a382055d5bb8c39d4
Ousssama El-Amrani <oussama.maroc8@gmail.com>
2025-12-27 18:42:58 +0100

    feat(Backend): enhance Kafka integration with new KStream function and configuration

--- src/main/java/me/elamranioussama/eventdriven/handlers/PageEventHandler.java
+++ src/main/java/me/elamranioussama/eventdriven/handlers/PageEventHandler.java
@@ -1,12 +1,17 @@
 package me.elamranioussama.eventdriven.handlers;
 
 import me.elamranioussama.eventdriven.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -29,5 +34,15 @@ public class PageEventHandler {
                         new Date(),
                         10 + new Random().nextLong(10000)
                 );
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>,KStream<String, Long>> KStreamFunction() {
+        return (input) -> input
+                .filter((k,v) -> v.duration() > 100)
+                .map((k,v) -> new KeyValue<>(v.name(), v.duration()))
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                .count()
+                .toStream();
     }
 }
